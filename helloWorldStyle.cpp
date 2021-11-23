caeabf5f94613aae0b2c1404459a2f3ea0ccda58
Ubuntu <ubuntu@ip-172-31-5-22.ec2.internal>
2021-11-23 20:24:28 +0000

    Created the helloWorldStyle.cpp file using style in C++

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Ina Tolo All rights reserved.
+// .
+// Created by: Ina Tolo
+// Date: Nov. 23, 2021
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+}
